61c7bc9b44750a7c68fc6e04798c1f065cd1965f
Matheus Felizardo <matheus.felizardo2@gmail.com>
2024-05-22 09:29:49 +0100

    updating config to get server url

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -16,6 +16,11 @@ export default defineNuxtConfig({
       autoprefixer: {},
     },
   },
+  runtimeConfig: {
+    public: {
+      baseUrl: process.env.NODE_ENV === "development" ? 'http://127.0.0.1:8000' : process.env.API_BASE_URL,
+    }
+  },
   // plugins: ['~/plugins/history.js'],
   
 })
