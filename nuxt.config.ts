4aa952b9a7bb95afcbc52489701b12a7959a53db
Matheus Felizardo <matheus.felizardo2@gmail.com>
2024-05-25 11:42:51 +0100

    adding api config in .env

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -17,5 +17,10 @@ export default defineNuxtConfig({
     },
   },
   // plugins: ['~/plugins/history.js'],
+  runtimeConfig: {
+    public: {
+      baseUrl: process.env.NODE_ENV === "development" ? 'http://127.0.0.1:8000' : process.env.API_BASE_URL,
+    }
+  },
   
 })
